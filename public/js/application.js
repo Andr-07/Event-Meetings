2a81a8c4f0acc71c534cfe07a8b8c2b04e26c2c3
melantagonista <frlelene@gmail.com>
2019-05-22 20:55:09 +0300

    changes2

---
+++ public/js/application.js
@@ -0,0 +1,5 @@
+let form1 = document.getElementById('form1');
+let form2 = document.getElementById('form2');
+let target = document.getElementsByName('target');
+let date = document.getElementsByName('date');
+
