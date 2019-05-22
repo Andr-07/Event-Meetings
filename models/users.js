c249402832853b1ca62fa0a8fc81a6cf67f09c78
filpoyma <filpoyma@gmail.com>
2019-05-22 18:15:09 +0300

    add userjs

--- models/users.js
+++ models/users.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String
+    name: String,
+    company: String,
+    position: String,
+    globalEvent: {
+        eventNames:String,
+        eventDate:Date
+    },
+    meetings: [{
+        invited: String,
+        status: Boolean,
+        target: String,
+        date: Date
+    }]
 });
 
 module.exports = mongoose.model('User', userSchema);
